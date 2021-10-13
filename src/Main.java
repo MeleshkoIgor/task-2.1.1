f6d97e3a0de55fd0a19f25a0f4179df3afaffeef
Igor Meleshko <igor.meleshko.1109@gmail.com>
2021-10-13 22:32:42 +0300

    initial task 2.1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2000;
+        int oneMile = 20;
+        int bonusMiles = ticketPrice / oneMile;
+
+        System.out.println("Количество миль" + bonusMiles);
+
+    }
+}
